566bb53e9f4ca51609da4554a96638526852eebc
robertgarrigos <robert@garrigos.cat>
2024-09-17 23:18:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Catalan Comarques Loader
+
+The Catalan Comarques extension for CiviCRM allows an easy method to load all the
+counties (comarques) in Catalaonia.  This extension is based in the US Counties
+extension by Andie Hunt.
+
+## Using the extension
+
+Enabling the extension runs the county loader, which loads all the counties in
+each province of Catalonia if they aren't already in your database.  From that
+point, the extension does nothing.  You can even disable the extension, and the
+counties will stay there.  Uninstalling the extension also does nothing, for an
+important reason: it could cause data loss by deleting data stored in addresses.
+
+## Changelog
+
+### Version 1.0
+
+Initial version
---
+++ comarques.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Check and load counties
+ */
+function comarques_loadcounties() {
+
+  // The counties.json file consists of JSON for one object where each key is
+  // the ISO code for a country, and each value is an object.  For each of those
+  // inner objects, each key is the name for a state/province and the value is
+  // an array of county names.
+  $allCounties = json_decode(file_get_contents(__DIR__ . '/comarques.json'), TRUE);
+
+  foreach ($allCounties as $countryIso => $counties) {
+    // Get array of states.
+    try {
+      $result = civicrm_api3('Country', 'getsingle', [
+        'iso_code' => $countryIso,
+        'api.Address.getoptions' => [
+          'field' => 'state_province_id',
+          'country_id' => '$value.id',
+          'sequential' => 0,
+        ],
+      ]);
+    }
+    catch (CiviCRM_API3_Exception $e) {
+      $error = $e->getMessage();
+      CRM_Core_Error::debug_log_message(ts('API Error: %1', [
+        'domain' => 'com.aghstrategies.uscounties',
+        1 => $error,
+      ]));
+      return FALSE;
+    }
+
+    if (empty($result['api.Address.getoptions']['values'])) {
+      return FALSE;
+    }
+    $states = $result['api.Address.getoptions']['values'];
+
+    // go state-by-state to check existing counties
+
+    foreach ($counties as $stateName => $state) {
+      $id = array_search($stateName, $states);
+      if ($id === FALSE) {
+        continue;
+      }
+
+      $check = "SELECT name FROM civicrm_county WHERE state_province_id = $id";
+      $results = CRM_Core_DAO::executeQuery($check);
+      $existing = [];
+      while ($results->fetch()) {
+        $existing[] = $results->name;
+      }
+
+      // identify counties needing to be loaded
+      $add = array_diff($state, $existing);
+
+      $insert = [];
+      foreach ($add as $county) {
+        $countye = CRM_Core_DAO::escapeString($county);
+        $insert[] = "('$countye', $id)";
+      }
+
+      // put it into queries of 50 counties each
+      for ($i = 0; $i < count($insert); $i = $i + 50) {
+        $inserts = array_slice($insert, $i, 50);
+        $query = "INSERT INTO civicrm_county (name, state_province_id) VALUES ";
+        $query .= implode(', ', $inserts);
+        CRM_Core_DAO::executeQuery($query);
+      }
+    }
+  }
+
+  return TRUE;
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function comarques_civicrm_install() {
+  comarques_loadcounties();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function comarques_civicrm_enable() {
+  comarques_loadcounties();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function comarques_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  comarques_loadcounties();
+}
